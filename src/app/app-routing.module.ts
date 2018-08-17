c753137007d593fd583bbb8e017995f01ccffd81
Errol Fernandes <errol.fernandes@atos.net>
2018-08-17 15:10:28 +0530

    added css
    Signed-off-by: Errol Fernandes <errol.fernandes@atos.net>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ServerlessIntroductionComponent } from './contents/serverless/serverless-introduction/serverless-introduction.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ServerlessIntroductionComponent } from './contents/serverless/serverless-introduction/serverless-introduction.component';
+import { BigDataBasicsComponent } from './contents/big-data/big-data-basics/big-data-basics.component';
+import { BlockchainBasicsComponent } from './contents/blockchain/blockchain-basics/blockchain-basics.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'serverless_basic', component: ServerlessIntroductionComponent }
+  { path: 'serverless_basic', component: ServerlessIntroductionComponent },
+  { path: 'big_data_basic', component: BigDataBasicsComponent },
+  { path: 'blockchain_basic', component: BlockchainBasicsComponent }
  
 ];
  
